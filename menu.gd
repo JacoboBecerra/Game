d28b45124368249a59de86a517d831719f771acf
JacoboBecerra <jbecerral23@fpcoruna.afundacion.org>
2024-11-09 18:29:24 +0100

    Menú funcional añadido

---
+++ menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$VBoxContainer/Start_game.grab_focus()
+	$AnimatedSprite2D.play("idle")
+	$Sun.play("idle")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_game_pressed() -> void:
+	get_tree().change_scene_to_file("res://Nivel1.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
