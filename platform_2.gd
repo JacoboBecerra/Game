d67a421ff3b763f0697e25af1ee26858ff1723db
JacoboBecerra <jbecerral23@fpcoruna.afundacion.org>
2024-11-24 11:18:49 +0100

    Diseño nivel 3 acabado

--- platform_2.gd
+++ platform_2.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 50.0
-var left_limit: float = 1710.0 #Límite izquierdo en la posición x
+var left_limit: float = 1955.0 #Límite izquierdo en la posición x
 var right_limit: float = 2230.0  # Límite derecho en la posición X
 
 
